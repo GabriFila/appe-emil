d8de1bba428bbbf36502aec7f28e4fbaede0a5da
Gabriele Filaferro <gabriele.filaferro@gmail.com>
2020-09-04 14:12:08 +0200

    Optmizied html, could also minify

--- peopleViewer/index.js
+++
@@ -1,9 +0,0 @@
-var db = firebase.firestore();
-var remPlaces = document.getElementById("remaining-places");
-var peopleData = {};
-db.collection("general_info")
-  .doc("people")
-  .onSnapshot((snap) => {
-    peopleData = snap.data();
-    remPlaces.innerText = peopleData.total - peopleData.present;
-  });
